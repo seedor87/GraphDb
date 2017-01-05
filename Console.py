f485b11ad3c661c2ab7041b9c19ae2d5d8c06099
Bill Clark <willowlark@outlook.com>
2017-01-05 14:51:40 -0500

    Comments

--- Console.py
+++ Console.py
@@ -7,6 +7,14 @@ import Graph_Access
 class Console(cmd.Cmd):
 
     def __init__(self, address):
+        """
+        `Author`: Bill Clark
+
+        Initializes the console to be used to interact with the graph.
+
+
+        `address`: The address of the graph to interact with.
+        """
         cmd.Cmd.__init__(self)
         self.manager = Graph_Access.GraphManager(address)
 
@@ -14,15 +22,37 @@ class Console(cmd.Cmd):
         self.subindex = None
 
     def do_load(self,data):
+        """
+        `Author`: Bill Clark
+
+        Creates an importer based on the given file and uses it
+        to import that file's nodes.
+
+        `data`: The filepath to the file to be loaded.
+        """
         f, other = self._split_path_data(data)
         importer = ImportFactory.get_importer(f)
         if importer is not None:
             self.manager.add_new(importer.import_file(f, other))
 
     def do_create(self, _):
+        """
+        `Author`: Bill Clark
+
+        Runs synchronize on the uncommitted nodes. This creates the
+         nodes created locally on the server.
+        """
         self.manager.sync_new()
 
     def do_print(self, line):
+        """
+        `Author`: Bill Clark
+
+        Print the subgraph at the index give in line. If line isn't
+        a digit, -1 is used which gets the uncommited index.
+
+        `line`: string of the integer index of subgraph to print.
+        """
         if line.isdigit(): line = int(line)
         else: line = -1
 
@@ -44,6 +74,13 @@ class Console(cmd.Cmd):
     #     self.subindex = line
 
     def do_find(self, _):
+        """
+        `Author`: Bill Clark
+
+        Interactive method that gets the values required to do a match
+        operation via cypher. It prints the index of the subgraph
+        match in the committed array.
+        """
         label, props = self._ask_node()
         rlabel = ""
         elabel, eprops = "", {}
@@ -56,12 +93,26 @@ class Console(cmd.Cmd):
         print self.manager.match_node(label, props, rlabel, elabel, eprops)
 
     def do_relate(self, line):
+        """
+        `Author`: Bill Clark
+
+        runs the relation bulding method in the graph manager.
+
+        `line`: the string of the index in committed to relate.
+        """
         if line.isdigit():
             self.manager.relate(int(line))
         else:
             print "Not an index."
 
     def do_upload(self, line):
+        """
+        `Author`: Bill Clark
+
+        Uploads the uncommitted subgraph to the server.
+
+        `line`: the string of the index in committed to relate.
+        """
         if line.isdigit():
             self.manager.upload_subgraph(int(line))
         else:
@@ -69,6 +120,15 @@ class Console(cmd.Cmd):
 
     @staticmethod
     def _ask_node():
+        """
+        `Author`: Bill Clark
+
+        Asks for the defining features of a node interactively.
+        Requires a label to be given, then takes any number of
+        keys and values for the node's properties.
+
+        `return`: The label and props dictionary.
+        """
         props = {}
         while True:
             label = raw_input("Input label to search with > ")
@@ -82,13 +142,17 @@ class Console(cmd.Cmd):
                 props[key] = val
         return label, props
 
-    def _set_graph_exists(self):
-        if self.subgraph is None:
-            return False
-        return True
-
     @staticmethod
     def _split_path_data(data):
+        """
+        `Author`: Bill Clark
+
+        Splits the string into a file address and the remainder.
+
+        `data`: The string to split.
+
+        `return`: The file address and the remaining string.
+        """
         r = re.match("([^.]*\..{3}) (.*)", data)
         return r.groups() if r else (data, None)
 
