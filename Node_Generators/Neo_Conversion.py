e8f42452de33c28e2e1f1815fd92bfa1d03a70f8
Bill Clark <willowlark@outlook.com>
2017-01-05 15:12:40 -0500

    COmments

--- Node_Generators/Neo_Conversion.py
+++ Node_Generators/Neo_Conversion.py
@@ -7,14 +7,49 @@ class NodeBuilder(object):
         pass
 
     def _generate(self, label, props):
+        """
+        `Author`: Bill Clark
+
+        Creates a node object and returns it.
+
+        `label`: the label of the node.
+
+        `props`: the property dictionary for the node.
+
+        `return`: the node generated.
+        """
         n = Node(label, **props)
         #for key, value in props.items():
         #    n[key] = value
         return n
 
     def make_node(self, label, props):
+        """
+        `Author`: Bill Clark
+
+        Calls the internal node generator.
+
+        `label`: the label of the node.
+
+        `props`: the property dictionary for the node.
+
+        `return`: the node generated.
+        """
         return self._generate(label, props)
 
     def add_new_node(self, label, props, subgraph=None):
+        """
+        `Author`: Bill Clark
+
+        Calls the internal node generator. When the node is retrieved,
+        it is either merged with the provided subgraph or returned
+        normally.
+
+        `label`: the label of the node.
+
+        `props`: the property dictionary for the node.
+
+        `return`: the node generated.
+        """
         ret = self.make_node(label, props)
         return ret if subgraph is None else ret | subgraph
