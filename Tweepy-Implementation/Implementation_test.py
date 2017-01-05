102ae677ed56cd8ef64a342fc8dabe4751c1e5b9
eliakah <kakoue66@students.rowan.edu>
2017-01-05 11:30:00 -0500

    updated csv manager

--- Tweepy-Implementation/Implementation_test.py
+++ Tweepy-Implementation/Implementation_test.py
@@ -2,7 +2,7 @@
 # Main.
 # This class runs all of the modules in the right sequential
 # order depending on the input
-
+from csv_manager import csv_manager
 import tweepy
 from time import sleep
 
@@ -12,7 +12,12 @@ def main():
     bot = TwitterBot("Z5dDoNOB1efZTIHsc3K39BTFa", "0OmTmSNH7whf7V06Z4LE3W4zXDAwxPKk9d948RLlQCBv7NgMyG",
                      "746388158-iY84sri3WGMULG9Hw3Q00ICurtA3pm8IMAho1gq7",
                      "H6oTFv58fmrkmaluiNTuBKxgpRnFK08hgtMH5c79u6ZEG")
-    bot.getTweetsFromHash("finals")
+    total = bot.getTweetsFromHash("me")
+    #manager = csv_manager("#", "testfile", "csv", "Screen Names, ID, Location, Tweet")
+    #manager.createFile()
+    #manager.add_row(total)  # output as csv
+
+
 
 
 # this class makes call to the twitter api
@@ -27,7 +32,9 @@ class TwitterBot:
     # this method prints the last 10 tweets from this hashtag
     def getTweetsFromHash(self, hashtag):
 
+
         hashtag = '#' + str(hashtag)
+        total = ""
 
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)  # authorising your access, login
         auth.set_access_token(self.access_token, self.access_token_secret)  # set access token
@@ -37,17 +44,23 @@ class TwitterBot:
         print(hashtag)
         # this just fetches the most recent entries for #
 
-        count = 1
+        count = 0
 
         for tweet in tweepy.Cursor(api.search, q=hashtag, lang='en').items():
-            if count == 11:
+            if count == 10:
                 break
             try:
-                print("Tweet #" + str(count))
-                print("Found tweet by: @" + tweet.user.screen_name)
-                print("Found tweet by user_id: " + str(tweet.user.id))
-                print("User Location: " + str(tweet.user.location))
-                print("Saying: '" + tweet.text + "'\n")
+                screenname = tweet.user.screen_name
+                id = str(tweet.user.id)
+                location = str(tweet.user.location)
+                theTweet = tweet.text
+
+                #print("Found tweet by: @" + screenname)
+                #print("Found tweet by user_id: " + id)
+                #print("User Location: " + location)
+                #print("Saying: '" + theTweet + "'\n")
+
+
 
 
             except tweepy.TweepError as e:
@@ -56,8 +69,11 @@ class TwitterBot:
                 continue
             except StopIteration:
                 break
-
+            total += "\n" +str(count + 1)+","+ screenname + "," + id + "," + location + ",\'" + theTweet + "\'"
             count += 1
 
+
+        print(total)
+        return total
 
 main()  # main method call
