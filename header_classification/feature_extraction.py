77528a332e4c59a15042a1cabab5a08d119e89bd
dales8 <dalesean2@gmail.com>
2017-01-17 17:58:01 -0500

    Implementing regex into the feature_classifier class.

--- header_classification/feature_extraction.py
+++ header_classification/feature_extraction.py
@@ -1,11 +1,19 @@
 
-import string
+import string, re
 
-select_chars = [':', '/', '.', '1', '2', '9', '0']
+select_expressions = ['(([0-1]\d|2[0-3]):[0-5]\d:[0-5]\d)(AM|PM)?', '((0\d|1[0-2])/([0-2]\d|3[0-1])/(\d{2}|\d{4}))',
+                      '(\+|\-)?(\d{2}\.\d+(N|S))', '(\+|\-)?(([0-8\d|90|1[0-7]\d|180)\.\d+(E|W))', '.+@.+\.\w+',
+                      '(\()?\d{3}(\))?( )?\d{3}(-)?\d{4}(\d{4})?',
+                      '((0\d|1[0-2])/([0-2]\d|3[0-1])/(\d{2}|\d{4})) (([0-1]\d|2[0-3]):[0-5]\d:[0-5]\d)(AM|PM)?']
 
 def extract_features(data):
     ret = {}
-    for char in select_chars:
-        ret['count: {}'.format(char)] = data.count(char)
+    for pattern in select_expressions:
+        expression = re.compile(pattern)
+        # if expression.match(data):
+        if re.search(expression, data):
+            ret['entry match at: {}'.format(pattern)] = 1
+        else:
+            ret['entry match at: {}'.format(pattern)] = 0
     ret['len'] = len(data)
     return ret
--- regEx/regular_expression_tests.py
+++ regEx/regular_expression_tests.py
@@ -1,29 +1,44 @@
 import re
-# from cryptography.fernet import Fernet
-#
-#
-#
-# #must make sure to install cryptography package (pip install cryptography)
-#
-# key = Fernet.generate_key()
-# cipher_suite = Fernet(key)
-# cipher_text = cipher_suite.encrypt(b"A really secret message. Not for prying eyes.") #encryption
-# plain_text = cipher_suite.decrypt(cipher_text) #decryption
+from cryptography.fernet import Fernet
 
-time = r"([0-2]\d:[0-5]\d:[0-5]\d)(AM|PM)" #regex for time, date, and latitude
-date = r"([0-1]\d/[0-3]\d/\d{4})"
-latitude = r"(\+|\-)?(\d{2}\.\d+.(N|S))"
-longitude = r"(\+|\-)?(\d{2}\.\d+.(E|W))"
+
+
+#must make sure to install cryptography package (pip install cryptography)
+
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+cipher_text = cipher_suite.encrypt(b"A really secret message. Not for prying eyes.") #encryption
+plain_text = cipher_suite.decrypt(cipher_text) #decryption
+
+cipher_text_time = cipher_suite.encrypt(b"([0-2]\d:[0-5]\d:[0-5]\d)(AM|PM)")
+cipher_text_date = cipher_suite.encrypt(b"([0-1]\d/[0-3]\d/\d{4})")
+cipher_text_latitude = cipher_suite.encrypt(b"(\+|\-)?(\d{2}\.\d+.(N|S))")
+cipher_text_longitude = cipher_suite.encrypt(b"(\+|\-)?(\d{2}\.\d+.(E|W))")
+cipher_text_email = cipher_suite.encrypt(b".+@.+\.\w+")
+
+time = r"(([0-1]\d|2[0-3]):[0-5]\d:[0-5]\d)(AM|PM)?" #regex for time, date, and latitude
+date = r"((0\d|1[0-2])/([0-2]\d|3[0-1])/(\d{2}|\d{4}))"
+date_time = r"((0\d|1[0-2])/([0-2]\d|3[0-1])/(\d{2}|\d{4})) (([0-1]\d|2[0-3]):[0-5]\d:[0-5]\d)(AM|PM)?"
+latitude = r"(\+|\-)?(\d{2}\.\d+(N|S))"
+longitude = r"(\+|\-)?(([0-8\d|90|1[0-7]\d|180)\.\d+(E|W))"
 email = r".+@.+\.\w+"
+phone = r"(\()?\d{3}(\))?( )?\d{3}(-)?\d{4}(\d{4})?"
 
 if re.search(time, "09:54:00PM"):
     print ("time OK")
 
-if re.search(date, "09/24/1990"):
+if re.search(date, "02/31/90"):
     print("date OK")
 
-if re.search(latitude, "+54.9864.N"):
+if re.search(date_time, "02/07/2006 10:40:43"):
+    print("date_time OK")
+
+if re.search(latitude, "+54.9864N"):
     print ("latitude OK")
 
-if re.search(longitude, "-33.9864.E"):
+if re.search(longitude, "-133.9864E"):
     print ("longitude OK")
+
+if re.search(phone, "(908) 797-7554"):
+    print ("phone OK")
+
