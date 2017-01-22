f12f09c132d427a952508e371b2fe1f256aed31b
Bob <seedor87@students.rowan.edu>
2017-01-22 15:40:38 -0500

    added organized structure to efficiently reproduce the csv upon read and classify technique. TODO now fix header skip bug

--- header_classification/header_normalization.py
+++ header_classification/header_normalization.py
@@ -6,15 +6,21 @@ local_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(_
 cm = classification_module()
 cm.instantiate_classifier()
 
-def has_header(file):
-    temp = csv.Sniffer().has_header(file.readline())
-    file.seek(0)
+def has_header(file_path):
+    """
+    TODO Fix this method it is in error now
+
+    :param file_path:
+    :return:
+    """
+    with open(file_path, 'rb') as file:
+        temp = csv.Sniffer().has_header(file.readline())
+        file.seek(0)
     return temp
 
 def determine_header(entries, shuffle=None, stop=0.1):
     """
-    TODO
-    improve the classification algorithm to be as accurate as possible
+
     """
     categories = defaultdict(int)
     if shuffle:
@@ -24,26 +30,39 @@ def determine_header(entries, shuffle=None, stop=0.1):
         categories[classification] += 1
     return max(categories.iteritems(), key=operator.itemgetter(1))[0]
 
-columns = defaultdict(list)
-with open(local_file_path, 'rb') as in_file:
-    reader = csv.reader(in_file, delimiter=',')
+def main(in_file_path, out_file_path=None, delimiter=','):
 
-    switch = has_header(in_file)
-    if switch:
-        next(reader)    # skip the row that is headers
-    hold_over = []
-    for row in reader:
-        hold_over.append(row)
-        for i in range(len(row)):
-            columns[i].append(row[i])
+    _out_file_path = in_file_path if out_file_path is None else out_file_path
 
-    headers = defaultdict(basestring)
-    for k, v in columns.iteritems():
-        headers[k] = determine_header(v)
-    headers = headers.values()
+    columns = defaultdict(list)
+    switch = has_header(in_file_path)
+    with open(in_file_path, 'rb') as in_file:
+        reader = csv.reader(in_file, delimiter=delimiter)
 
-with open(local_file_path, 'wb') as out_file:
-    writer = csv.writer(out_file)
-    writer.writerow(headers)
-    for row in hold_over:
-        writer.writerow(row)
+        # skip the row that is headers
+        if switch:
+            next(reader)
+
+        # Build new data struct of dictionary of columns
+        for row in reader:
+            for i in range(len(row)):
+                columns[i].append(row[i])
+
+        # determine what new headers will be, stored in fieldnames
+        fieldnames = defaultdict(basestring)
+        for k, v in columns.iteritems():
+            fieldnames[k] = determine_header(v)
+        fieldnames = fieldnames.values()
+
+        # reconstruct rows from columns
+        new_l = zip(*columns.values())
+
+        # Write the rows one by one out to the optional out_file_path
+        with open(_out_file_path, 'wb') as out_file:
+            writer = csv.writer(out_file)
+            writer.writerow(fieldnames)
+            for row in new_l:
+                writer.writerow(row)
+
+if __name__ == '__main__':
+    main(local_file_path)
